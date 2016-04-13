3767b40714378e75149ba4dfd0cdbcdf10c69ee4
Tyler Nakamura <tyler@tylernakamura.com>
2016-04-13 13:44:06 -1000

    update tip-calculator to take user input

--- tip-calculator.py
+++ tip-calculator.py
@@ -1,5 +1,10 @@
-def calculateTip(total, rate):
-    return total * rate
 
-print "The tip is "
-print calculateTip(37.50, .15)
+#takes in user input and prints out the tip for the total bsaed on the rate
+def calculateTip():
+    total = float(input("Please enter the total amount as a float: "))
+    rate  = float(input("Please enter the tax rate: "))
+    tip = total * rate
+    print "The tip is: ", tip 
+    return tip
+
+calculateTip()
