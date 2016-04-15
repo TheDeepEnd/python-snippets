6adb430c8f8c3c05746b3077c50136ec11e443e2
Tyler Nakamura <tyler@tylernakamura.com>
2016-04-15 13:36:06 -1000

    add comments

--- clay_tip_calculator.py
+++ clay_tip_calculator.py
@@ -1,14 +1,19 @@
-
-#takes in user input and prints out the tip for the total bsaed on the rate
+#method that takes in user input and prints out the tip for the total based on the rate
 def calculateTip():
     try:
+	#ask user for input
         total = float(raw_input("Please enter the total amount: "))
         rate  = float(raw_input("Please enter the tax rate (ie .15): "))
     except:
+	#the user has entered an invalid value
         print "Whoops, please enter a correct value next time!"
         exit()
+    #calculate tip
     tip = total * rate
+
+    #print tip
     print "The tip is: ", tip
     return tip
 
+#call method
 calculateTip()
